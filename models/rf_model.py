493a0712b9ab2b4d1a88d0518fc8091974ccc059
yankur <42523950+yankur@users.noreply.github.com>
2024-07-03 16:34:33 +0300

    input size for fitting fixed

--- models/rf_model.py
+++ models/rf_model.py
@@ -7,7 +7,7 @@ class RFModel(DigitClassificationInterface):
     def __init__(self):
         self.model = RandomForestClassifier(n_estimators=10, random_state=42)
         # fit the model with random data
-        dummy_X = np.random.rand(100, 724)
+        dummy_X = np.random.rand(100, 100)
         dummy_y = np.random.randint(0, 10, 100)
         self.model.fit(dummy_X, dummy_y)
 
